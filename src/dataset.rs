ec67d9b8b24ea4fa5e60e942f784c9e5469f0d75
John Foley <jfoley@cs.umass.edu>
2019-08-01 14:53:48 -0400

    fix bug in RF, should be sampling by keeping queries together.
    Signed-off-by: John Foley <jfoley@cs.umass.edu>

--- src/dataset.rs
+++ src/dataset.rs
@@ -163,25 +163,43 @@ pub trait DatasetSampling {
     /// At least one feature and one instance is selected no matter how small the percentage.
     fn random_sample<R: Rng>(&self, frate: f64, srate: f64, rand: &mut R) -> SampledDatasetRef;
 
+    /// This represents a deterministic sampling of instances.
     fn with_instances(&self, instances: &[u32]) -> SampledDatasetRef;
+
+    /// This represents a determinisitc sampling of queries.
+    fn with_queries(&self, queries: &[String]) -> SampledDatasetRef;
+
+    fn train_test<R: Rng>(
+        &self,
+        test_fraction: f64,
+        rand: &mut R,
+    ) -> (SampledDatasetRef, SampledDatasetRef);
 }
 
 impl DatasetSampling for &dyn RankingDataset {
     fn random_sample<R: Rng>(&self, frate: f64, srate: f64, rand: &mut R) -> SampledDatasetRef {
         let features = self.features();
-        let instances = self.instances();
+        let queries = self.queries();
 
         let n_features = cmp::max(1, ((features.len() as f64) * frate) as usize);
-        let n_instances = cmp::max(1, ((instances.len() as f64) * srate) as usize);
+        let n_queries = cmp::max(1, ((queries.len() as f64) * srate) as usize);
 
         let features = features
             .choose_multiple(rand, n_features)
             .cloned()
             .collect();
-        let instances = instances
-            .choose_multiple(rand, n_instances)
-            .cloned()
+        let queries: HashSet<&str> = queries
+            .choose_multiple(rand, n_queries)
+            .map(|s| s.as_str())
             .collect();
+        
+        let mut instances: Vec<u32> = Vec::new();
+
+        for (qid, qinst) in self.instances_by_query() {
+            if queries.contains(qid.as_str()) {
+                instances.extend(qinst);
+            }
+        }
 
         SampledDatasetRef {
             parent: self.get_ref(),
@@ -195,6 +213,47 @@ impl DatasetSampling for &dyn RankingDataset {
             instances: instances.iter().cloned().collect(),
             features: self.features(),
         }
+    }
+
+    fn with_queries(&self, queries: &[String]) -> SampledDatasetRef {
+        let query_set: HashSet<&str> = queries.iter().map(|s| s.as_str()).collect();
+        let mut instances: Vec<u32> = Vec::new();
+
+        for (qid, qinst) in self.instances_by_query() {
+            if query_set.contains(qid.as_str()) {
+                instances.extend(qinst);
+            }
+        }
+
+        SampledDatasetRef {
+            parent: self.get_ref(),
+            instances,
+            features: self.features(),
+        }
+    }
+
+    fn train_test<R: Rng>(
+        &self,
+        test_fraction: f64,
+        rand: &mut R,
+    ) -> (SampledDatasetRef, SampledDatasetRef) {
+        let mut qs = self.queries();
+        let n_test_qs = ((qs.len() as f64) * test_fraction) as usize;
+        if n_test_qs <= 0 {
+            panic!(
+                "Must be some testing data selected: frac={}!",
+                test_fraction
+            );
+        }
+        if n_test_qs >= qs.len() {
+            panic!("Must be some training data left!");
+        }
+
+        qs.shuffle(rand);
+        let test_qs = qs.split_off(n_test_qs);
+        let train_qs = qs;
+
+        (self.with_queries(&train_qs), self.with_queries(&test_qs))
     }
 }
 
