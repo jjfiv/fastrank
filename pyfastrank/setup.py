255f807bef0599c4e30e7b40ca873797a0a8194b
John Foley <jjfoley@smith.edu>
2019-08-07 09:29:32 -0400

    try version 0.2.0 to test travis deploy

--- pyfastrank/setup.py
+++ pyfastrank/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fastrank',
-    version='0.1.0',
+    version='0.2.0',
     author="John Foley",
     author_email="jjfoley@smith.edu",
     description="A set of learning-to-rank algorithms.",
