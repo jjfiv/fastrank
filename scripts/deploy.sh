d9bb96ec26b90659a4eb4e6e38a1eec9dcfe6645
John Foley <jjfoley@smith.edu>
2019-08-07 09:00:32 -0400

    pypi api was changed user is now __token__

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,5 @@ set -eu
 pip3 install --user --upgrade pip
 pip3 install --user pyo3-pack
 
-cd cfastrank && pyo3-pack publish -b cffi -u @token -p PYPI_CFASTRANK_TOKEN && cd -
+cd cfastrank && pyo3-pack publish -b cffi -u __token__ -p PYPI_CFASTRANK_TOKEN && cd -
 
