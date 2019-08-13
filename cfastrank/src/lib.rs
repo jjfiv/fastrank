5022a4eea760d5eeda4a611359de1dca84a1872a
John Foley <jfoley@cs.umass.edu>
2019-08-13 16:31:09 -0400

    support access to libsvm part of library in python -- much more cffi code :(
    Signed-off-by: John Foley <jfoley@cs.umass.edu>

--- cfastrank/src/lib.rs
+++ cfastrank/src/lib.rs
@@ -4,16 +4,165 @@ use serde_json;
 #[macro_use]
 extern crate serde_derive;
 use fastrank::coordinate_ascent::CoordinateAscentParams;
+use fastrank::dataset;
+use fastrank::dataset::DatasetRef;
+use fastrank::dataset::RankingDataset;
 use fastrank::dense_dataset::DenseDataset;
 use fastrank::json_api::*;
 use std::error::Error;
 use std::ffi::{CStr, CString};
-use std::mem;
+use std::ptr;
 use std::slice;
+
+#[derive(Serialize, Deserialize)]
+struct ErrorMessage {
+    error: String,
+    context: String,
+}
+
+pub struct CDataset {
+    /// Reference to Rust-based Dataset.
+    reference: DatasetRef,
+}
+
+#[repr(C)]
+pub struct CResult {
+    pub error_message: *const c_void,
+    pub success: *const c_void,
+}
+
+impl Default for CResult {
+    fn default() -> Self {
+        CResult {
+            error_message: ptr::null(),
+            success: ptr::null(),
+        }
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn free_str(originally_from_rust: *mut c_void) {
     let _will_drop: CString = unsafe { CString::from_raw(originally_from_rust as *mut c_char) };
+}
+
+/// Note: not-recursive. Free Error Message Manually!
+#[no_mangle]
+pub extern "C" fn free_c_result(originally_from_rust: *mut CResult) {
+    let _will_drop: Box<CResult> = unsafe { Box::from_raw(originally_from_rust) };
+}
+
+#[no_mangle]
+pub extern "C" fn free_dataset(originally_from_rust: *mut CDataset) {
+    let _will_drop: Box<CDataset> = unsafe { Box::from_raw(originally_from_rust) };
+}
+
+/// Internal helper: convert string reference to pointer to be passed to Python/C. Heap allocation.
+fn return_string(output: &str) -> *const c_void {
+    let c_output: CString = CString::new(output).expect("Conversion to CString should succeed!");
+    CString::into_raw(c_output) as *const c_void
+}
+
+#[no_mangle]
+pub extern "C" fn load_ranksvm_format(
+    data_path: *mut c_void,
+    feature_names_path: *mut c_void,
+) -> *const CResult {
+    let data_path: &CStr = unsafe { CStr::from_ptr(data_path as *mut c_char) };
+    let feature_names_path: Option<&CStr> = if feature_names_path.is_null() {
+        None
+    } else {
+        Some(unsafe { CStr::from_ptr(feature_names_path as *mut c_char) })
+    };
+    let mut c_result = Box::new(CResult::default());
+    match result_load_ranksvm_format(data_path, feature_names_path) {
+        Ok(response) => {
+            let c_dataset = Box::new(CDataset {
+                reference: response,
+            });
+            c_result.success = Box::into_raw(c_dataset) as *const c_void;
+        }
+        Err(e) => {
+            let error_message = serde_json::to_string(&ErrorMessage {
+                error: "error".to_string(),
+                context: format!("{:?}", e),
+            })
+            .unwrap();
+            c_result.error_message = return_string(&error_message);
+        }
+    };
+    Box::into_raw(c_result)
+}
+
+fn result_load_ranksvm_format(
+    data_path: &CStr,
+    feature_names_path: Option<&CStr>,
+) -> Result<DatasetRef, Box<Error>> {
+    let data_path: &str = data_path
+        .to_str()
+        .map_err(|_| "Could not convert your data_path string to UTF-8!")?;
+    let feature_names = feature_names_path
+        .map(|s| {
+            s.to_str()
+                .map_err(|_| "Could not convert your feature_names_path string to UTF-8!")
+        })
+        .transpose()?
+        .map(|path| dataset::load_feature_names_json(path))
+        .transpose()?;
+    Ok(DatasetRef::load_libsvm(data_path, feature_names.as_ref())?)
+}
+
+#[no_mangle]
+pub extern "C" fn query_dataset_json(
+    dataset: *mut c_void,
+    json_cmd_str: *mut c_void,
+) -> *const c_void {
+    let dataset: Option<&CDataset> = unsafe { (dataset as *mut CDataset).as_ref() };
+    let json_cmd_str: &CStr = unsafe { CStr::from_ptr(json_cmd_str as *mut c_char) };
+    let output = match result_query_dataset_json(dataset, json_cmd_str) {
+        Ok(response) => response,
+        Err(e) => serde_json::to_string(&ErrorMessage {
+            error: "error".to_string(),
+            context: format!("{:?}", e),
+        })
+        .unwrap(),
+    };
+    let c_output: CString = CString::new(output).expect("Conversion to CString should succeed!");
+    CString::into_raw(c_output) as *const c_void
+}
+
+fn result_query_dataset_json(
+    dataset: Option<&CDataset>,
+    query_str: &CStr,
+) -> Result<String, Box<Error>> {
+    let dataset = match dataset {
+        Some(d) => d,
+        None => Err("Dataset pointer is null!")?,
+    };
+    let query_str: &str = query_str
+        .to_str()
+        .map_err(|_| "Could not convert your query string to UTF-8!")?;
+
+    let response = match query_str {
+        "num_features" => serde_json::to_string(&dataset.reference.n_dim())?,
+        "feature_ids" => serde_json::to_string(&dataset.reference.features())?,
+        "num_instances" => serde_json::to_string(&dataset.reference.instances().len())?,
+        "queries" => serde_json::to_string(&dataset.reference.queries())?,
+        "feature_names" => {
+            let names = dataset
+                .reference
+                .features()
+                .into_iter()
+                .map(|f| dataset.reference.feature_name(f))
+                .collect::<Vec<_>>();
+            serde_json::to_string(&names)?
+        }
+        other => serde_json::to_string(&ErrorMessage {
+            error: "unknown_dataset_query_str".to_owned(),
+            context: other.to_owned(),
+        })?,
+    };
+
+    Ok(response)
 }
 
 #[no_mangle]
@@ -29,12 +178,6 @@ pub extern "C" fn query_json(json_cmd_str: *mut c_void) -> *const c_void {
     };
     let c_output: CString = CString::new(output).expect("Conversion to CString should succeed!");
     CString::into_raw(c_output) as *const c_void
-}
-
-#[derive(Serialize, Deserialize)]
-struct ErrorMessage {
-    error: String,
-    context: String,
 }
 
 fn result_exec_json(query_str: &CStr) -> Result<String, Box<Error>> {
--- pyfastrank/fastrank/__init__.py
+++ pyfastrank/fastrank/__init__.py
@@ -10,37 +10,7 @@ from sklearn.datasets import load_svmlight_file
 from typing import Dict, List
 
 
-@attr.s
-class CoordinateAscentParams(object):
-    num_restarts = attr.ib(type=int, default=5)
-    num_max_iterations = attr.ib(type=int, default=25)
-    step_base = attr.ib(type=float, default=0.05)
-    step_scale = attr.ib(type=float, default=2.0)
-    tolerance = attr.ib(type=float, default=0.001)
-    seed = attr.ib(type=int, default=random.randint(0,1 << 64))
-    normalize = attr.ib(type=bool, default=True)
-    quiet = attr.ib(type=bool, default=False)
-    init_random = attr.ib(type=bool, default=True)
-    output_ensemble = attr.ib(type=bool, default=False)
-
-@attr.s
-class QueryJudgments(object):
-    docid_to_rel: Dict[str,float] = attr.ib(factory=dict)
-
-@attr.s
-class QuerySetJudgments(object):
-    query_to_judgments: Dict[str, QueryJudgments] = attr.ib(factory=dict)
-
-@attr.s
-class TrainRequest(object):
-    measure = attr.ib(type=str, default="ndcg")
-    params = attr.ib(type=CoordinateAscentParams, factory=CoordinateAscentParams)
-    judgments = attr.ib(type=QuerySetJudgments, default=None)
-
-
-
-
-def claim_rust_str(result) -> str:
+def _handle_rust_str(result) -> str:
     """
     This method decodes bytes to UTF-8 and makes a new python string object. 
     It then frees the bytes that Rust allocated correctly.
@@ -53,6 +23,126 @@ def claim_rust_str(result) -> str:
         lib.free_str(result)
 
 
+def _handle_c_result(c_result):
+    """
+    This handles the logical-OR struct of the CDataset { error_message, success } 
+    where both the wrapper and the error_message will be freed by the end of this function.
+
+    The success pointer is returned or an error is raised!
+    """
+    if c_result == ffi.NULL:
+        raise ValueError("CResult should not be NULL")
+    error = None
+    success = None
+    if c_result.error_message != ffi.NULL:
+        error = _handle_rust_str(c_result.error_message)
+    if c_result.success != ffi.NULL:
+        success = c_result.success
+    lib.free_c_result(c_result)
+    _maybe_raise_error_str(error)
+    return success
+
+
+def _maybe_raise_error_str(rust_error_string):
+    if rust_error_string is None:
+        return
+    if "{" in rust_error_string:
+        response = json.loads(rust_error_string)
+        if "error" in response and "context" in response:
+            raise Exception("{0}: {1}".format(response["error"], response["context"]))
+    else:
+        raise Exception(rust_error_string)
+
+
+def _maybe_raise_error_json(response):
+    if response is None:
+        return
+    if isinstance(response, dict):
+        if "error" in response and "context" in response:
+            raise Exception("{0}: {1}".format(response["error"], response["context"]))
+    return
+
+
+class CDataset(object):
+    def __init__(self, pointer=None):
+        self.pointer = pointer
+
+    def __del__(self):
+        if self.pointer is not None:
+            lib.free_dataset(self.pointer)
+            self.pointer = None
+
+    def open_ranksvm(self, data_path, feature_names_path=None):
+        if self.pointer is not None:
+            raise ValueError("Cannot call open twice!")
+        data_path = data_path.encode("utf-8")
+        if feature_names_path is not None:
+            feature_names_path = feature_names_path.encode("utf-8")
+        else:
+            feature_names_path = ffi.NULL
+        self.pointer = _handle_c_result(
+            lib.load_ranksvm_format(data_path, feature_names_path)
+        )
+        print(self.pointer)
+
+    def __query_json(self, message="num_features"):
+        if self.pointer is None:
+            raise ValueError("Forgot to call open_* on CDataset!")
+        response = json.loads(
+            _handle_rust_str(
+                lib.query_dataset_json(self.pointer, message.encode("utf-8"))
+            )
+        )
+        _maybe_raise_error_json(response)
+        return response
+
+    def num_features(self):
+        return self.__query_json("num_features")
+
+    def feature_ids(self):
+        return self.__query_json("feature_ids")
+
+    def feature_names(self):
+        return self.__query_json("feature_names")
+
+    def num_instances(self):
+        return self.__query_json("num_instances")
+
+    def queries(self):
+        return self.__query_json("queries")
+
+
+@attr.s
+class CoordinateAscentParams(object):
+    num_restarts = attr.ib(type=int, default=5)
+    num_max_iterations = attr.ib(type=int, default=25)
+    step_base = attr.ib(type=float, default=0.05)
+    step_scale = attr.ib(type=float, default=2.0)
+    tolerance = attr.ib(type=float, default=0.001)
+    seed = attr.ib(type=int, default=random.randint(0, 1 << 64))
+    normalize = attr.ib(type=bool, default=True)
+    quiet = attr.ib(type=bool, default=False)
+    init_random = attr.ib(type=bool, default=True)
+    output_ensemble = attr.ib(type=bool, default=False)
+
+
+@attr.s
+class QueryJudgments(object):
+    docid_to_rel: Dict[str, float] = attr.ib(factory=dict)
+
+
+@attr.s
+class QuerySetJudgments(object):
+    query_to_judgments: Dict[str, QueryJudgments] = attr.ib(factory=dict)
+
+
+@attr.s
+class TrainRequest(object):
+    measure = attr.ib(type=str, default="ndcg")
+    params = attr.ib(type=CoordinateAscentParams, factory=CoordinateAscentParams)
+    judgments = attr.ib(type=QuerySetJudgments, default=None)
+
+
 def query_json(message: str) -> str:
     """
     This method sends any old python object as input into the Rust exec_json call.
@@ -60,25 +150,40 @@ def query_json(message: str) -> str:
     Returns a string.
     """
     command = message.encode("utf-8")
-    response = json.loads(claim_rust_str(lib.query_json(command)))
-    if 'error' in response and 'context' in response:
-        raise Exception('{0}: {1}'.format(response['error'], response['context']))
-
+    response = json.loads(_handle_rust_str(lib.query_json(command)))
+    _maybe_raise_error_json(response)
     return response
 
 
 #%%
 if __name__ == "__main__":
+    rd = CDataset()
+    rd.open_ranksvm("../examples/trec_news_2018.train")
+    print(
+        "num_features: {0}, num_instances: {1}, queries: {2} features: {3}".format(
+            rd.num_features(), rd.num_instances(), rd.queries(), rd.feature_names()
+        )
+    )
+    rd2 = CDataset()
+    rd2.open_ranksvm(
+        "../examples/trec_news_2018.train", "../examples/trec_news_2018.features.json"
+    )
+    print(
+        "num_features: {0}, num_instances: {1}, queries: {2} features: {3}".format(
+            rd.num_features(), rd.num_instances(), rd.queries(), rd.feature_names()
+        )
+    )
+
     print(TrainRequest())
 
     train_req = query_json("coordinate_ascent_defaults")
-    ca_params = train_req['params']['CoordinateAscent']
-    ca_params['init_random'] = True
-    ca_params['seed'] = 42
-    ca_params['quiet'] = True
+    ca_params = train_req["params"]["CoordinateAscent"]
+    ca_params["init_random"] = True
+    ca_params["seed"] = 42
+    ca_params["quiet"] = True
     print(train_req)
-    
-    train_X, train_y, train_qid = load_svmlight_file(
+
+    (train_X, train_y, train_qid) = load_svmlight_file(
         "../examples/trec_news_2018.train",
         dtype=np.float32,
         zero_based=False,
@@ -87,13 +192,17 @@ if __name__ == "__main__":
     train_X = train_X.todense()
     (train_N, train_D) = train_X.shape
     print(train_N, train_D)
-    train_req_str = json.dumps(train_req).encode('utf-8')
-    train_resp = json.loads(claim_rust_str(lib.train_dense_dataset_f32_f64_i64(
-        train_req_str,
-        train_N,
-        train_D,
-        ffi.cast("float *", train_X.ctypes.data),
-        ffi.cast("double *", train_y.ctypes.data),
-        ffi.cast("int64_t *", train_qid.ctypes.data),
-    )))
+    train_req_str = json.dumps(train_req).encode("utf-8")
+    train_resp = json.loads(
+        _handle_rust_str(
+            lib.train_dense_dataset_f32_f64_i64(
+                train_req_str,
+                train_N,
+                train_D,
+                ffi.cast("float *", train_X.ctypes.data),
+                ffi.cast("double *", train_y.ctypes.data),
+                ffi.cast("int64_t *", train_qid.ctypes.data),
+            )
+        )
+    )
     print(train_resp)
