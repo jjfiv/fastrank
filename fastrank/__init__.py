9d09d698e5e97c4b3b8ddad9d91a59e074225832
John Foley <johnf@middlebury.edu>
2021-04-09 09:54:44 -0400

    try killing the __all__ list since I'm not smart enough?

--- fastrank/__init__.py
+++ fastrank/__init__.py
@@ -5,5 +5,3 @@ from .models import CoordinateAscentRanker, RandomForestRanker, ImportedRanker, 
 
 VERSION_TUPLE = (0,8,0)
 __version__ = '{}.{}.{}'.format(*VERSION_TUPLE)
-
-__all__ = ['clib', 'models', 'training', 'ImportedRanker', 'make_dataset', 'CoordinateAscentRanker', 'RandomForestRanker', 'CQRel', 'CDataset', 'CModel', 'query_json', 'TrainRequest']
