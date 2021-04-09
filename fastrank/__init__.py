c9459c40457654d4ca676c526b5b40af58671f1a
John Foley <johnf@middlebury.edu>
2021-04-09 09:50:17 -0400

    assume the problem is with __all__?

--- fastrank/__init__.py
+++ fastrank/__init__.py
@@ -1,9 +1,9 @@
 #%%
 from .clib import CQRel, CDataset, CModel, query_json
 from .training import TrainRequest
-from .models import CoordinateAscentRanker, RandomForestRanker
+from .models import CoordinateAscentRanker, RandomForestRanker, ImportedRanker, make_dataset
 
-VERSION_TUPLE = (0,7,0)
+VERSION_TUPLE = (0,8,0)
 __version__ = '{}.{}.{}'.format(*VERSION_TUPLE)
 
-__all__ = ['clib', 'training', 'CoordinateAscentRanker', 'RandomForestRanker', 'CQRel', 'CDataset', 'CModel', 'query_json', 'TrainRequest']
+__all__ = ['clib', 'models', 'training', 'ImportedRanker', 'make_dataset', 'CoordinateAscentRanker', 'RandomForestRanker', 'CQRel', 'CDataset', 'CModel', 'query_json', 'TrainRequest']
