dbe836b0905a9f79cb413b273a45afd04b0d5a76
John Foley <johnf@middlebury.edu>
2021-04-09 09:16:11 -0400

    new ranker tests, pytest

--- fastrank/models.py
+++ fastrank/models.py
@@ -47,12 +47,15 @@ class CoordinateAscentRanker:
             raise ValueError("Cannot inspect weights before fit.")
         return np.array(self.model.to_dict()['Linear']['weights'])
 
+
+
 class RandomForestRanker:
     def __init__(self, measure: str = 'ndcg', params: RandomForestParams = RandomForestParams()):
         self.params = params
         self.measure = measure
         self.model: Optional[CModel] = None
         self.qrel: Optional[CQRel] = None
+
 
     def fit(self, X: np.ndarray, y: np.ndarray, qids: List[str]):
         dataset = make_dataset(X, y, qids)
@@ -73,3 +76,77 @@ class RandomForestRanker:
             raise ValueError("Cannot score before fit.")
         dataset = make_dataset(X, y, qids)
         return np.array(self.model.predict_dense_scores(dataset))
+
+    @staticmethod
+    def from_sklearn(m: Any, measure: str = 'ndcg', params: RandomForestParams = RandomForestParams()) -> 'RandomForestRanker':
+
+        forest = RandomForestRanker(measure, params)
+        return forest
+
+class ImportedRanker:
+    def __init__(self, model: CModel):
+        self.model = model
+
+    def score(self, X: np.ndarray, y: np.ndarray, qids: List[str], measure: str, qrel: Optional[CQRel] = None) -> float:
+        dataset = make_dataset(X, y, qids)
+        by_query = dataset.evaluate(self.model, measure, qrel)
+        if len(by_query) == 0:
+            return 0.0
+        return sum(by_query.values()) / len(by_query)
+
+    def predict(self, X: np.ndarray, y: np.ndarray, qids: List[str]) -> np.ndarray:
+        dataset = make_dataset(X, y, qids)
+        return np.array(self.model.predict_dense_scores(dataset))
+    
+    @staticmethod
+    def from_sklearn(m: Any, expected_feature_dim: Optional[int] = None) -> 'ImportedRanker':
+        """
+        Ingest a sklearn classifier or regressor as a ranker (if possible).
+        Tested with:
+          - SGDClassifier / SGDRegressor (other linear models should work)
+          - RandomForestClassifier / RandomForestRegressor
+          - ExtraTreesClassifier / ExtraTreesRegressor
+          - GradientBoostingClassifier / etc.
+        """
+        if hasattr(m, 'coef_'):
+            weights = m.coef_.flatten()
+            if expected_feature_dim:
+                assert(len(weights) == expected_feature_dim)
+            as_ranker = CModel.from_dict({"Linear": {"weights": weights.tolist()}})
+            return ImportedRanker(as_ranker)
+        if hasattr(m, 'estimators_'):
+            est = m.estimators_
+            if isinstance(est, np.ndarray):
+                est = est.tolist()
+            models = [sklearn_tree_to_json(t) for t in est]
+            ensemble = {"Ensemble": {
+                "models": models,
+                "weights": [1.0 for _ in models]
+            }}
+            return ImportedRanker(CModel.from_dict(ensemble))
+        raise ValueError("Not sure how to create a ImportedRanker from {}".format(m))
+        
+
+def sklearn_tree_to_json(tree_model):
+    """Recursively turn a SKLearn Tree model into a python dictionary (which can be saved as JSON)"""
+    if isinstance(tree_model, list):
+        tree_model = tree_model[0]
+    tree = tree_model.tree_
+
+    from sklearn.tree import _tree
+
+    def recurse(node, depth=0):
+        """Recursively handle a given node."""
+        if tree.feature[node] != _tree.TREE_UNDEFINED:
+            fid = int(tree.feature[node])
+            threshold = float(tree.threshold[node])
+            return {"FeatureSplit": {
+                "fid": fid,
+                "split": threshold,
+                "lhs": recurse(tree.children_left[node], depth+1),
+                "rhs": recurse(tree.children_right[node], depth+1),
+            }}
+        else:
+            return {"LeafNode": tree.value[node][0].tolist()[0]}
+
+    return {"DecisionTree": recurse(0)}
---
+++ tests/test_ranker_api.py
@@ -0,0 +1,54 @@
+import fastrank
+from fastrank.models import *
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingClassifier, GradientBoostingRegressor
+from sklearn.datasets import load_svmlight_file
+
+(X, y, qid) = load_svmlight_file("examples/trec_news_2018.train",
+        dtype=np.float32,
+        zero_based=False,
+        query_id=True,
+    )
+X = X.todense()
+
+measure = 'ndcg'
+RAND = 1234
+
+def test_rf():
+    rf = RandomForestRanker(measure)
+    rf.params.seed = RAND
+    rf.params.quiet = True
+    rf.fit(X, y, qid)
+    assert rf.score(X, y, qid) > 0.70
+
+def test_sklearn_rf():
+    rf = RandomForestRegressor(random_state=RAND)
+    rf.fit(X, y)
+    rf = ImportedRanker.from_sklearn(rf)
+    assert rf.score(X, y, qid, measure) > 0.85
+
+def test_sklearn_gbc():
+    rf = GradientBoostingClassifier(random_state=RAND)
+    rf.fit(X, y > 0)
+    rf = ImportedRanker.from_sklearn(rf)
+    assert rf.score(X, y, qid, measure) > 0.70
+
+def test_sklearn_gbr():
+    rf = GradientBoostingRegressor(random_state=RAND)
+    rf.fit(X, y)
+    rf = ImportedRanker.from_sklearn(rf)
+    assert rf.score(X, y, qid, measure) > 0.70
+
+def test_ca():
+    ca = CoordinateAscentRanker(measure)
+    ca.params.quiet = True
+    ca.params.seed = RAND
+    ca.fit(X, y, qid)
+    assert ca.score(X, y, qid) > 0.7
+
+def test_sklearn_lr():
+    sk = LinearRegression()
+    sk.fit(X, y)
+    lr = ImportedRanker.from_sklearn(sk)
+    assert lr.score(X, y, qid, measure) > 0.74
+
